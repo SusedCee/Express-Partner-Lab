2230d22759169adc99102dcdc0b9d4a6ddd2e53c
Sused Cabrera <susedcabrera@Suseds-MacBook-Pro.local>
2019-07-17 12:32:48 -0600

    ignore and dog array

---
+++ models/dog.js
@@ -0,0 +1,27 @@
+const dog = [
+	{
+		name: "Shenzie", 
+		size: "small",
+		color: "brindel",
+		spayed/neutered: true
+	},
+	{
+		name: "Beyonce", 
+		size: "small",
+		color: "caramel",
+		spayed/neutered: true
+	},
+		{
+		name: "Simba", 
+		size: "medium",
+		color: "golden",
+		spayed/neutered: true
+	},
+		{
+		name: "Cosmo", 
+		size: "small",
+		color: "golden",
+		spayed/neutered: false
+	}
+];
+
